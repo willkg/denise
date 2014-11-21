f62e4b2af0528efbc63e76d0bc1ccf0cbaee370e
Will Kahn-Greene <willkg@mozilla.com>
2014-11-21 11:29:38 -0500

    Tweak forms for less wasted vertical space

--- denise/static/js/denise.js
+++ denise/static/js/denise.js
@@ -1,1 +1,24 @@
-// daas javascript goes here
+(function($, document) {
+    'use strict';
+    $(document).ready(function() {
+        // From http://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3/
+        $(document).on('change', '.btn-file :file', function() {
+            var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+            input.trigger('fileselect', [numFiles, label]);
+        });
+
+        $(document).ready(function() {
+            $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+                var input = $(this).parents('.input-group').find(':text'),
+                    log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+                if( input.length ) {
+                    input.val(log);
+                }
+
+            });
+        });
+    });
+}(jQuery, document));
